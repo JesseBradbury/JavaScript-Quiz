c437663d8831412cf5b4fe7fdb5a4a267a78231c
JB <bradburyjesse7@gmail.com>
2023-11-09 23:18:59 -0700

    Added timer and button functionality.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,7 @@
 
 // We land on a start page with a Title, Description, Start Button
 // create event for when start button is pressed
-var startDiv = document.querySelector(".start");
+var startDiv = document.querySelector("#start-page");
 startDiv.setAttribute("style", "text-align: center")
 
 var title = document.createElement("h1");
@@ -10,10 +10,12 @@ title.textContent = "JavaScript Basics Quiz";
 title.setAttribute("style", "text-align: center; font-size: 3em; font-weight: bold");
 
 var description = document.createElement("p");
+description.setAttribute("id", "start-text");
 description.textContent = "This is a description of the quiz. If you get one wrong, blah blah blah... It will keep your score at the end of the game. Good Luck!";
 description.setAttribute("style", "text-align: center; font-size: 12px; font-weight: normal");
 
 var startButton = document.createElement("button");
+startButton.setAttribute("id", "start")
 startButton.textContent = "Start Quiz!";
 // Snagged these styles from the previous project
 // TODO: Style this differently. 
@@ -23,8 +25,29 @@ startDiv.appendChild(title);
 startDiv.appendChild(description);
 startDiv.appendChild(startButton);
 
-// we move to the first quiz question, 
+// Timer and Button Logic
+var timer = document.querySelector('#timer');
+var startBtn = document.querySelector('#start');
+var startText = document.querySelector('#start-page');
+var questionContainer = document.querySelector('#quiz');
+
 // the timer goes to 75
+let time = 75;
+// This is the button press events. Sets the display of the start-page section to hidden
+// sets the question container page to visible. 
+startBtn.addEventListener("click", function(){
+startText.classList.add("hidden")
+
+questionContainer.classList.remove("hidden")
+
+timer.textContent = time;
+let timerInterval = setInterval(function () {
+    time--;
+    timer.textContent = time;
+}, 1000)
+    
+})
+
 // question text is displayed with 4 multiple choice answers, this will be pulled from an array
 // If the user selects the correct answer, we move to the next question. 
 // if the user selects the incorrect question the time is dedcuted 15 seconds and we move to the next question. 
