df5f144ed34c16c593ad5f153f7dbfe24390b86f
JB <bradburyjesse7@gmail.com>
2023-11-13 23:24:32 -0700

    added highscore.html and logic that links the submit button to the new page.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,7 @@
 // We land on a start page with a Title, Description, Start Button
 // create event for when start button is pressed
 var startDiv = document.querySelector("#start-page");
-startDiv.setAttribute("style", "text-align: center")
+startDiv.setAttribute("style", "text-align: center");
 
 var title = document.createElement("h1");
 title.textContent = "JavaScript Basics Quiz";
@@ -41,74 +41,16 @@ let time = 75;
 
 var timerInterval;
 
-startBtn.addEventListener("click", function () {
-    startText.classList.add("hidden")
-
-    questionContainer.classList.remove("hidden")
-    questionContainer.setAttribute("style", "text-align: center; margin: auto; display: flex; flex-direction: column; align-items: center;");
-
-    timer.textContent = time;
-    timerInterval = setInterval(function () {
-        time--;
-        timer.textContent = time;
-
-        if (time <= 0) {
-            clearInterval(timerInterval);
-        }
-    }, 1000)
-
-    displayQuestion();
-})
-
-
-// question text is displayed with 4 multiple choice answers, this will be pulled from an array
-var storedQuestions = JSON.parse(localStorage.getItem("questions"));
-var questionIndex = 0;
-
-var questionContainer = document.getElementById("quiz");
-
-function displayQuestion() {
-
-    questionContainer.innerHTML = "";
-
-    var desiredQuestion = storedQuestions[questionIndex].question;
-    var prompt = document.createElement("h3")
-    prompt.textContent = desiredQuestion;
-    prompt.setAttribute("style", "text-align: center");
-    prompt.setAttribute("id", "quiz");
-    questionContainer.appendChild(prompt);
-
-    // If the user selects the correct answer, we move to the next question. 
-    // if the user selects the incorrect question the time is dedcuted 15 seconds and we move to the next question. 
-    // Loop this for the length of the array of questions.  5 Questions?
-
-    // This function checks if the correct answer was selected, if not it subtracts 10 seconds. 
-    // Once the last question is selected, it starts the function for showing the score page. 
-
-    function handleButtonClick() {
-        console.log("handleButtonClick called");
-        var correctIndex = storedQuestions[questionIndex].correctIndex;
-
-        // TODO: Add a notificaton for correct or incorrect respopnse. 
-        if (this.textContent !== storedQuestions[questionIndex].choices[correctIndex]) {
-            time -= 10;
-        }
-
-        if (questionIndex < storedQuestions.length - 1) {
-            questionIndex++;
-            displayQuestion();
-        } else {
-            clearInterval(timerInterval);
-            showScorePage();
-            console.log("Quiz completed!");
-        }
+function checkTimeAndShowScore() {
+    if (time <= 0) {
+        clearInterval(timerInterval);
+        showScorePage();
     }
+}
 
-
-
-    // This is the function that runs after the quiz
+  // This is the function that runs after the quiz
     // This will start the score section.
-    function showScorePage() {
+function showScorePage() {
         // debugger
         var quizSection = document.getElementById("quiz");
         var scoreSection = document.getElementById("score");
@@ -134,12 +76,14 @@ function displayQuestion() {
             var userTime = time;
             storeScore(userName, userTime);
 
-            displayHighScores();
+            window.location.href = "highscore.html"
+            // displayHighScores();
         });
 
         scoreSection.appendChild(nameInput);
         scoreSection.appendChild(submitButton);
 
+        var timeHeadder = document.querySelector(".time-header")
         // Hides quiz section and shows Score page
         quizSection.classList.add("hidden");
         quizSection.setAttribute("style", "display: none");
@@ -149,8 +93,7 @@ function displayQuestion() {
 
     }
 
-    var timeHeadder = document.querySelector(".time-header");
-
+    // This is the Function that will store the score and name in local storage
     function storeScore(userName, userTime) {
         var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
@@ -159,12 +102,153 @@ function displayQuestion() {
             time: userTime
         };
         highScores.push(newScore);
-        highScores.sort(function (a, b) {
+        highScores.sort(function (b, a) {
             return a.time - b.time;
         });
 
         localStorage.setItem("highScores", JSON.stringify(highScores));
     }
+
+// This is the function that will start the quiz when the user hits the startBtn
+startBtn.addEventListener("click", function () {
+    startText.classList.add("hidden")
+
+    questionContainer.classList.remove("hidden")
+    questionContainer.setAttribute("style", "text-align: center; margin: auto; display: flex; flex-direction: column; align-items: center;");
+
+    timer.textContent = time;
+    timerInterval = setInterval(function () {
+        time--;
+        timer.textContent = time;
+
+        checkTimeAndShowScore();
+        // if (time <= 0) {
+        //     clearInterval(timerInterval);
+        // }
+    }, 1000)
+
+    displayQuestion();
+})
+
+
+// question text is displayed with 4 multiple choice answers, this will be pulled from an array
+var storedQuestions = JSON.parse(localStorage.getItem("questions"));
+var questionIndex = 0;
+
+var questionContainer = document.getElementById("quiz");
+
+function displayQuestion() {
+
+    questionContainer.innerHTML = "";
+
+    // Creates the question
+    var desiredQuestion = storedQuestions[questionIndex].question;
+    var prompt = document.createElement("h3")
+    prompt.textContent = desiredQuestion;
+    prompt.setAttribute("style", "text-align: center");
+    prompt.setAttribute("id", "quiz");
+    questionContainer.appendChild(prompt);
+
+    // If the user selects the correct answer, we move to the next question. 
+    // if the user selects the incorrect question the time is dedcuted 15 seconds and we move to the next question. 
+    // Loop this for the length of the array of questions.  5 Questions?
+
+    // This function checks if the correct answer was selected, if not it subtracts 10 seconds. 
+    // Once the last question is selected, it starts the function for showing the score page. 
+
+    // Creates the multiple choice answer buttons
+    // for (var i = 0; i < answers.length; i++) {
+
+    //     var answerButton = document.createElement("button");
+    //     answerButton.classList.add("question-button");
+    //     answerButton.textContent = answers[i];
+
+    //     answerButton.addEventListener("click", handleButtonClick);
+
+
+    //     questionContainer.appendChild(answerButton);
+    // }
+
+    function handleButtonClick() {
+        console.log("handleButtonClick called");
+        var correctIndex = storedQuestions[questionIndex].correctIndex;
+
+        // TODO: Add a notificaton for correct or incorrect respopnse. 
+        if (this.textContent !== storedQuestions[questionIndex].choices[correctIndex]) {
+            time -= 10;
+        }
+
+        if (questionIndex < storedQuestions.length - 1) {
+            questionIndex++;
+            displayQuestion();
+        } else {
+            clearInterval(timerInterval);
+            showScorePage();
+            console.log("Quiz completed!");
+        }
+    }
+
+
+
+    // This is the function that runs after the quiz
+    // This will start the score section.
+    // function showScorePage() {
+    //     // debugger
+    //     var quizSection = document.getElementById("quiz");
+    //     var scoreSection = document.getElementById("score");
+
+    //     clearInterval(timerInterval);
+    //     var scoreHeading = document.createElement("h2");
+    //     scoreHeading.textContent = "All Done!";
+    //     scoreSection.appendChild(scoreHeading);
+
+    //     var userTime = time;
+    //     var timeDisplay = document.createElement("p");
+    //     timeDisplay.textContent = "Your Time: " + userTime + " seconds";
+    //     scoreSection.appendChild(timeDisplay);
+
+    //     var nameInput = document.createElement("input");
+    //     nameInput.setAttribute("type", "text");
+    //     nameInput.setAttribute("placeholder", "Enter Your Name");
+
+    //     var submitButton = document.createElement("button");
+    //     submitButton.textContent = "Submit";
+    //     submitButton.addEventListener("click", function () {
+    //         var userName = nameInput.value;
+    //         var userTime = time;
+    //         storeScore(userName, userTime);
+
+    //         displayHighScores();
+    //     });
+
+    //     scoreSection.appendChild(nameInput);
+    //     scoreSection.appendChild(submitButton);
+
+    //     // Hides quiz section and shows Score page
+    //     quizSection.classList.add("hidden");
+    //     quizSection.setAttribute("style", "display: none");
+    //     scoreSection.classList.remove("hidden");
+    //     quizSection.innerHTML = "";
+    //     timeHeadder.classList.add("hidden");
+
+    // }
+
+    // var timeHeadder = document.querySelector(".time-header")
+
+    // function storeScore(userName, userTime) {
+    //     var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+    //     var newScore = {
+    //         name: userName,
+    //         time: userTime
+    //     };
+    //     highScores.push(newScore);
+    //     highScores.sort(function (a, b) {
+    //         return a.time - b.time;
+    //     });
+
+    //     localStorage.setItem("highScores", JSON.stringify(highScores));
+    // }
 
     function displayHighScores() {
         var highScores = JSON.parse(localStorage.getItem("scores")) || [];
@@ -173,6 +257,7 @@ function displayQuestion() {
 
     var answers = storedQuestions[questionIndex].choices;
 
+    // Creates the multiple choice answer buttons
     for (var i = 0; i < answers.length; i++) {
 
         var answerButton = document.createElement("button");
